0a0a5eafedfdcee9997dd4074e43aa6956777678
Wuzak <wuzak7@gmail.com>
2024-04-30 21:49:12 +0300

    Add FSM

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,19 +1,32 @@
 from aiogram import Router
-from aiogram.filters import Command, CommandStart
+from aiogram.filters import Command, CommandStart, StateFilter
 from aiogram.types import Message
+from aiogram.fsm.state import State, StatesGroup, default_state
+
+from keyboards.main_kb import main_kb
+
+
+class FSMCurrencyConverter(StatesGroup):
+    first_currency = State()   # Состояние ожидания выбора конвертируемой валюты
+    second_currency = State()  # Состояние ожидания выбора валюты, в которую необходимо конвертировать
+    quantity = State()         # Состояние ожидания ввода количества конвертируемой валюты
+
 
 router = Router()
 
 
-@router.message(CommandStart)
+@router.message(CommandStart(), StateFilter(default_state))
 async def start_command(message: Message):
-    await message.answer(text=f'Привет, <b>{message.from_user.first_name}</b>!\nЧем я могу помочь?')
+    await message.answer(
+        text=f'Привет, <b>{message.from_user.first_name}</b>!\nЧем я могу помочь?',
+        reply_markup=main_kb
+    )
 
 
 @router.message(Command(commands='help'))
 async def help_command(message: Message):
     await message.answer(
-        text=f'Кнопка "<b>Курс</b>" предоставит Вам актуальный перечень '
+        text=f'Кнопка <b>Курс</b> предоставит Вам актуальный перечень '
              f'курсов всех валют, выраженных в российских рублях.\n\n'
-             f'"<b>Конвертер</b>" позволяет преобразовать заданное количество денежных единиц из одной валюты в другую'
+             f'<b>Конвертер</b> позволяет преобразовать заданное количество денежных единиц из одной валюты в другую'
     )
