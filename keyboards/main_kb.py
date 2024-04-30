8cd2216ca8c01553c3428d24898d31f333de6da3
Wuzak <wuzak7@gmail.com>
2024-04-30 17:32:25 +0300

    Add main_kb.py

---
+++ keyboards/main_kb.py
@@ -0,0 +1,16 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+rates_button = InlineKeyboardButton(
+    text='Курс',
+    callback_data='rates_button'
+)
+
+converter_button = InlineKeyboardButton(
+    text='Конвертер',
+    callback_data='converter_button'
+)
+
+main_kb = InlineKeyboardMarkup(
+    inline_keyboard=[[rates_button],
+                     [converter_button]]
+)
