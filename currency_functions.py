d853a0af641f17573a5021ec0572863fc5323886
Wuzak <wuzak7@gmail.com>
2024-05-01 12:44:52 +0300

    Add function that returns exchange rates

--- currency_functions.py
+++ currency_functions.py
@@ -1,8 +1,21 @@
 import requests
 
+from currencies import currencies
+
 
 def get_data():
     url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 
     return requests.get(url).json()
 
+
+def get_exchange_rates():
+    rates = []
+    data = get_data()
+
+    for code, name in currencies.items():
+        rates.append(f'{name[0]} {code} {data["Valute"][code]["Value"]}')
+
+    result = '\n'.join(rates)
+
+    return result
