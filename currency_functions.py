edf65913c03cd635cb63aa8bf7fb79a8358fcb59
Wuzak <wuzak7@gmail.com>
2024-05-02 20:51:05 +0300

    Fix bug with frequent calls to API

--- currency_functions.py
+++ currency_functions.py
@@ -9,19 +9,19 @@ def get_data():
     return requests.get(url).json()
 
 
-def get_value(code):
-    data = get_data()
+def get_value(code, data):
 
     return data["Valute"][code]["Value"] / data["Valute"][code]["Nominal"]
 
 
 def get_exchange_rates():
     rates = []
+    data = get_data()
 
     for code, name in currencies.items():
         if code == 'RUB':
             continue
-        value = round(get_value(code), 3)
+        value = round(get_value(code, data), 3)
         rates.append(f'{name[0]} {code}  {value}')
 
     result = '\n'.join(rates)
@@ -30,8 +30,10 @@ def get_exchange_rates():
 
 
 def currency_converter(first, second, quantity):
-    first_value = get_value(first)
-    second_value = get_value(second)
+    data = get_data()
+
+    first_value = get_value(first, data)
+    second_value = get_value(second, data)
     result = round(first_value / second_value, 3) * quantity
 
     return f'{quantity} {currencies[first][0]} {first} = {result} {currencies[second][0]} {second}'
